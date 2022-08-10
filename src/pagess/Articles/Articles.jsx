8b35adbbe8ef14a559acd62a3508de3859bb588f
Nikhil Malviya <94277199+nikk422@users.noreply.github.com>
2022-08-10 17:40:25 +0530

    fix bugs

--- src/pagess/Articles/Articles.jsx
+++ src/pagess/Articles/Articles.jsx
@@ -7,39 +7,35 @@ const Articles = () => {
   const { articleId } = useParams();
   const { data } = useArticle();
 
+  const articlesData = data.find((item) => item.id === parseInt(articleId));
+
   return (
     <main>
       <Navbar />
-      {data.map((item) => {
-        if (item.id === parseInt(articleId)) {
-          return (
-            <div className="container-news flex-column justify-center">
-              <h2 className="title">{item.title.rendered}</h2>
-              <section className="author-detail flex-column">
-                <p>
-                  written by :-
-                  <strong>{item.parsely.meta.author[0].name}</strong>
-                </p>
-                <p>
-                  updated :- <strong className="date">{item.date}</strong>
-                </p>
-              </section>
-              <section className="artilcle-img-contain">
-                <img
-                  src={item.jetpack_featured_media_url}
-                  className="article-img2"
-                  alt="logo"
-                />
-              </section>
-              <section className="content">
-                <p
-                  dangerouslySetInnerHTML={{ __html: item.content.rendered }}
-                ></p>
-              </section>
-            </div>
-          );
-        }
-      })}
+      <div className="container-news flex-column justify-center">
+        <h2 className="title">{articlesData?.title.rendered}</h2>
+        <section className="author-detail flex-column">
+          <p>
+            written by :-
+            <strong>{articlesData?.parsely.meta.author[0].name}</strong>
+          </p>
+          <p>
+            updated :- <strong className="date">{articlesData?.date}</strong>
+          </p>
+        </section>
+        <section className="artilcle-img-contain">
+          <img
+            src={articlesData?.jetpack_featured_media_url}
+            className="article-img2"
+            alt="logo"
+          />
+        </section>
+        <section className="content">
+          <p
+            dangerouslySetInnerHTML={{ __html: articlesData?.content.rendered }}
+          ></p>
+        </section>
+      </div>
     </main>
   );
 };
